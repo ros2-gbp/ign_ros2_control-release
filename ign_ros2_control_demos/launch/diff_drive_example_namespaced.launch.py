928b4d305b0e64f34fafc21ebda573df278b4928
Alejandro Hernandez Cordero <ahcorde@gmail.com>
2025-02-07 10:04:33 +0100

    Rebase from 'upstream'

--- ign_ros2_control_demos/launch/diff_drive_example_namespaced.launch.py
+++ ign_ros2_control_demos/launch/diff_drive_example_namespaced.launch.py
@@ -1,4 +1,4 @@
-# Copyright 2024 ros2_control Development Team
+# Copyright 2024 Open Source Robotics Foundation, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,8 +12,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, ExecuteProcess, IncludeLaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch.actions import RegisterEventHandler
 from launch.event_handlers import OnProcessExit
 from launch.launch_description_sources import PythonLaunchDescriptionSource
@@ -30,74 +34,80 @@ def generate_launch_description():
     # Get URDF via xacro
     robot_description_content = Command(
         [
-            PathJoinSubstitution([FindExecutable(name="xacro")]),
-            " ",
+            PathJoinSubstitution([FindExecutable(name='xacro')]),
+            ' ',
             PathJoinSubstitution(
-                [FindPackageShare("ign_ros2_control_demos"),
-                 "urdf", "test_pendulum_effort.xacro.urdf"]
+                [FindPackageShare('ign_ros2_control_demos'),
+                 'urdf', 'test_diff_drive.xacro.urdf']
             ),
+            ' ',
+            'namespace:=r1',
         ]
     )
-    robot_description = {"robot_description": robot_description_content}
+    robot_description = {'robot_description': robot_description_content}
 
     node_robot_state_publisher = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
+        namespace='r1',
         output='screen',
-        parameters=[robot_description]
+        parameters=[robot_description, {'use_sim_time': use_sim_time}],
     )
 
-    gz_spawn_entity = Node(
+    ignition_spawn_entity = Node(
         package='ros_gz_sim',
         executable='create',
+        namespace='r1',
         output='screen',
-        arguments=["-topic", "robot_description",
-                   "-name", "cart", "-allow_renaming", "true"],
+        arguments=['-topic', 'robot_description',
+                   '-name', 'diff_drive',
+                   '-allow_renaming', 'true'],
     )
 
-    load_joint_state_broadcaster = ExecuteProcess(
-        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
-             'joint_state_broadcaster'],
-        output='screen'
+    joint_state_broadcaster_spawner = Node(
+        package='controller_manager',
+        executable='spawner',
+        arguments=[
+            'joint_state_broadcaster',
+            '-c', '/r1/controller_manager'
+            ],
     )
-
-    load_joint_effort_controller = ExecuteProcess(
-        cmd=['ros2', 'control', 'load_controller',
-             '--set-state', 'active', 'effort_controller'],
-        output='screen'
+    diff_drive_base_controller_spawner = Node(
+        package='controller_manager',
+        executable='spawner',
+        arguments=[
+            'diff_drive_base_controller',
+            '-c', '/r1/controller_manager'
+            ],
     )
 
     # Bridge
     bridge = Node(
         package='ros_gz_bridge',
         executable='parameter_bridge',
-        arguments=['/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock'],
+        arguments=['/clock@rosgraph_msgs/msg/Clock[ignition.msgs.Clock'],
         output='screen'
     )
 
     return LaunchDescription([
+        bridge,
         # Launch gazebo environment
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource(
-                [PathJoinSubstitution([FindPackageShare('ros_gz_sim'),
-                                       'launch',
-                                       'gz_sim.launch.py'])]),
-            launch_arguments=[('gz_args', [' -r -v 3 empty.sdf'])]),
+                [os.path.join(get_package_share_directory('ros_ign_gazebo'),
+                              'launch', 'ign_gazebo.launch.py')]),
+            launch_arguments=[('gz_args', [' -r -v 4 empty.sdf'])]),
         RegisterEventHandler(
             event_handler=OnProcessExit(
-                target_action=gz_spawn_entity,
-                on_exit=[load_joint_state_broadcaster],
+                target_action=ignition_spawn_entity,
+                on_exit=[
+                          joint_state_broadcaster_spawner,
+                          diff_drive_base_controller_spawner
+                        ],
             )
         ),
-        RegisterEventHandler(
-            event_handler=OnProcessExit(
-                target_action=load_joint_state_broadcaster,
-                on_exit=[load_joint_effort_controller],
-            )
-        ),
-        bridge,
         node_robot_state_publisher,
-        gz_spawn_entity,
+        ignition_spawn_entity,
         # Launch Arguments
         DeclareLaunchArgument(
             'use_sim_time',
