29270b91b8331c2714a6a52efccb057bb7b1b653
Alejandro Hernandez Cordero <ahcorde@gmail.com>
2026-05-13 10:43:35 +0200

    Rebase from 'debian/humble/gz_ros2_control_demos'

--- launch/gripper_mimic_joint_example_position.launch.py
+++ launch/gripper_mimic_joint_example_position.launch.py
@@ -1,4 +1,4 @@
-# Copyright 2024 ros2_control Development Team
+# Copyright 2024 Open Source Robotics Foundation, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,6 +11,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
+# Author: Denis Stogl (Stogl Robotics Consulting)
+#
 
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
@@ -34,7 +37,7 @@ def generate_launch_description():
             ' ',
             PathJoinSubstitution(
                 [FindPackageShare('gz_ros2_control_demos'),
-                 'urdf', 'test_pendulum_position.xacro.urdf']
+                 'urdf', 'test_gripper_mimic_joint_position.xacro.urdf']
             ),
         ]
     )
@@ -43,7 +46,7 @@ def generate_launch_description():
         [
             FindPackageShare('gz_ros2_control_demos'),
             'config',
-            'cart_controller_position.yaml',
+            'gripper_controller_position.yaml',
         ]
     )
 
@@ -58,8 +61,8 @@ def generate_launch_description():
         package='ros_gz_sim',
         executable='create',
         output='screen',
-        arguments=['-topic', 'robot_description',
-                   '-name', 'cart', '-allow_renaming', 'true'],
+        arguments=['-topic', 'robot_description', '-name',
+                   'gripper', '-allow_renaming', 'true'],
     )
 
     joint_state_broadcaster_spawner = Node(
@@ -67,11 +70,11 @@ def generate_launch_description():
         executable='spawner',
         arguments=['joint_state_broadcaster'],
     )
-    joint_trajectory_controller_spawner = Node(
+    gripper_controller_spawner = Node(
         package='controller_manager',
         executable='spawner',
         arguments=[
-            'joint_trajectory_controller',
+            'gripper_controller',
             '--param-file',
             robot_controllers,
             ],
@@ -102,7 +105,7 @@ def generate_launch_description():
         RegisterEventHandler(
             event_handler=OnProcessExit(
                 target_action=joint_state_broadcaster_spawner,
-                on_exit=[joint_trajectory_controller_spawner],
+                on_exit=[gripper_controller_spawner],
             )
         ),
         bridge,
